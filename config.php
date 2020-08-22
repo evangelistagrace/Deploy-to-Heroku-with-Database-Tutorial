48371f8aaad72ad6dfbc303953fa0954c93c8d41
Grace <evangrace98@gmail.com>
2020-08-22 23:27:01 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $host = 'ec2-3-213-102-175.compute-1.amazonaws.com';
 $port = 5432;
 $dbname = 'dc3u745blljbg3';
 $user = 'klkpurjzqcnpuh';
-$password = '    b243fa85dce4e9b46894f946fdb31917e875565cfc2ae795f90ee21b4760b280';
+$password = 'b243fa85dce4e9b46894f946fdb31917e875565cfc2ae795f90ee21b4760b280';
 
 $db = pg_connect(
     "
